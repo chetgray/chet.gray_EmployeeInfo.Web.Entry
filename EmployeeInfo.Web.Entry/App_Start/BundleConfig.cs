c85606fe11fada1ecdf4b704a491eadbd8f199ff
Chet Gray <chet.gray@waystar.com>
2023-03-17 13:01:13 -0400

    🏗️ build: add/include custom.js bundle

--- EmployeeInfo.Web.Entry/App_Start/BundleConfig.cs
+++ EmployeeInfo.Web.Entry/App_Start/BundleConfig.cs
@@ -7,6 +7,8 @@ namespace EmployeeInfo.Web.Entry
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/custom").Include("~/Scripts/custom.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Site.css"));
         }
     }
