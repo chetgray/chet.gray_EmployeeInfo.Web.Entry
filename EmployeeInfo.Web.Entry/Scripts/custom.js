58eaecddd4b22bc108441cffa7fe92b680ecc58b
Chet Gray <chet.gray@waystar.com>
2023-03-17 17:40:09 -0400

    ✨ feat: handle department drop-down selection

--- EmployeeInfo.Web.Entry/Scripts/custom.js
+++ EmployeeInfo.Web.Entry/Scripts/custom.js
@@ -1,10 +1,21 @@
 ﻿// @ts-check
 
 $(function () {
-    $("#submitButton").on("click", function (event) {
+    /** @type JQuery<HTMLFormElement> */
+    const $employeeForm = $("#employeeInfoEntryForm");
+
+    $employeeForm.on("submit", handleSubmit);
+    $employeeForm.find("#departmentDropMenu a[data-department]").on("click", setDepartment);
+
+    function handleSubmit(event) {
         event.preventDefault();
         // TODO: handle submit
         // @ts-ignore
         showAlert("Entry Accepted.", "Success");
-    });
+    }
+
+    function setDepartment(event) {
+        event.preventDefault();
+        $("input#employeeDepartment").val($(this).data("department"));
+    }
 });
